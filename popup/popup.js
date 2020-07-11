5491dd55d5f7a38050a2c5cdceb7f89e9ece7c14
Ranjan <tejasvi.ranjan@nokia.com>
2020-07-11 15:38:14 +0530

    some more changes

--- popup/popup.js
+++ popup/popup.js
@@ -2,12 +2,13 @@ var parent = document.querySelector(".model-parent"),
     button = document.getElementById("btn1"),
     x = document.querySelector(".x");
     section = document.querySelector("section")
-    body = document.querySelector()
+    body = document.querySelector("body")
 
 button.addEventListener("click", appearFunc)
 
 function appearFunc(){
     parent.style.display = "block";
+    
     section.style.transition = "0.5s";
     section.style.filter = "blur(10px)"; 
 
