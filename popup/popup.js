1af9398d92322292d00639a9f8ba7bc762cc3ce5
Ranjan <tejasvi.ranjan@nokia.com>
2020-07-11 19:40:19 +0530

    added details about me

--- popup/popup.js
+++ popup/popup.js
@@ -1,28 +1,52 @@
 var parent = document.querySelector(".model-parent"),
+    model = document.querySelector(".model")
+    model1 = document.querySelector(".model1")
     button = document.getElementById("btn1"),
+    abt_btn = document.getElementById("btn2")
     x = document.querySelector(".x");
+    x1 = document.querySelector(".x1");
+
     section = document.querySelector("section")
     body = document.querySelector("body")
 
 button.addEventListener("click", appearFunc)
+abt_btn.addEventListener("click", abtbtnFunc)
+
+
 
 function appearFunc(){
+    //fuction for button 1
     parent.style.display = "block";
-    
+    model.style.display = "block";
+    model1.style.display = "None";
     section.style.transition = "0.5s";
     section.style.filter = "blur(10px)"; 
 
 }
 
+
+function abtbtnFunc(){
+    //function for button 2
+    parent.style.display = "block";
+    model.style.display = "none";
+    model1.style.display = "block";
+    section.style.transition = "0.5s";
+    section.style.filter = "blur(10px)"; 
+}
+
+
 x.addEventListener("click", dissappear)
+x1.addEventListener("click", dissappear)
+
 
 function dissappear(){
     parent.style.display = "none";
     section.style.transition = "0.5s";
     section.style.filter = "blur(0px)";
     
-    
 }
+
+
 
 parent.addEventListener("click", dissappearParent)
 
