adffe72f70a38f910f12bc3b05205aa42d198b5d
Ranjan <tejasvi.ranjan@nokia.com>
2020-07-11 10:56:14 +0530

    basic blur and dropdown

---
+++ popup/popup.js
@@ -0,0 +1,35 @@
+var parent = document.querySelector(".model-parent"),
+    button = document.getElementById("btn1"),
+    x = document.querySelector(".x");
+    section = document.querySelector("section")
+    body = document.querySelector()
+
+button.addEventListener("click", appearFunc)
+
+function appearFunc(){
+    parent.style.display = "block";
+    section.style.transition = "0.5s";
+    section.style.filter = "blur(10px)"; 
+
+}
+
+x.addEventListener("click", dissappear)
+
+function dissappear(){
+    parent.style.display = "none";
+    section.style.transition = "0.5s";
+    section.style.filter = "blur(0px)";
+    
+    
+}
+
+parent.addEventListener("click", dissappearParent)
+
+function dissappearParent(e){
+    if (e.target.className=="model-parent"){
+        parent.style.display = "none";
+        section.style.transition = "0.5s";
+        section.style.filter = "blur(0px)";
+    }
+
+}
